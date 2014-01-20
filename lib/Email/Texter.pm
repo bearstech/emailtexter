7341b4ac512388416e677d6405875c59353589ff
zerodeux <vcaron@bearstech.com>
2014-01-20 19:51:36 +0100

    Switched from HTML::Striper to HTML::Parser + custom SAX-based stripper (using better knowledge of HTML email structure)

--- lib/Email/Texter.pm
+++ lib/Email/Texter.pm
@@ -17,7 +17,7 @@
 package Email::Texter;
 
 use Email::MIME;
-use HTML::Strip;
+use HTML::Parser;
 
 use Exporter 'import';
 @EXPORT = qw/ email_texter /;
@@ -27,20 +27,63 @@ sub email_texter {
 
   my $text;
   my $html;
+  # Parse MIME parts from end to begin in order that the first ones take precedence
   foreach (reverse $email->parts) {
     my $type = $_->content_type;
     $text = $_->body_str if $type =~ m:^text/plain;?: or $type eq '';
     $html = $_->body_str if $type =~ m:^text/html;?:;
   }
-  die "No text or HTML MIME part found, exiting" unless defined $text or defined $html;
+  # No text or HTML MIME part found, we leave the email untouched
+  return unless defined $text or defined $html;
  
-  if (not defined $text) {
-    # Strip Thunderbird signature while it's easy
-    $html =~ s/<pre class="moz-signature".*//s;
+  # If there's an HTML part, use it. Wich means we prefer HTML over text when
+  # an email has both text+HTML as multipart/alternative, because it's more
+  # structured and it's easier to remove signatures and quotes
+  if (defined $html) {
+    # Strip known signature blogs while it's easy
+    $html =~ s:<pre class="moz-signature".*::s;                       # Thunderbird
+    $html =~ s:<div class="gmail_quote">.*?\n</blockquote></div>::sg; # Gmail
  
-    my $hs = HTML::Strip->new();
-    $text = $hs->parse($html);
-    $hs->eof;
+    my @ts;        # tag stack
+    my $trim = 0;  # we trim an HTML subtree if $trim > 0
+    my $tt = \$text;
+    $text = '';
+    my $p = HTML::Parser->new( api_version => 3,
+      unbroken_text => 1,
+      start_h => [ \&start_h, 'tagname,attr' ],
+      text_h  => [ \&text_h, 'dtext' ],
+      end_h   => [ \&end_h ],
+    );
+    sub start_h {
+      my ($name, $attr) = @_;
+      push(@ts, $name);
+      print STDERR "TAG: ", join(' ', @ts), " > ", join(' ', map { "$_='$attr->{$_}'" } keys %$attr), "\n" if defined $ENV{debug};
+      if ($name eq 'div' and $attr->{class} eq 'gmail_quote') {
+        $trim++;
+        push(@ts, '_trim_'); # Mark in the stack when we started trimming
+        print STDERR "$trim TRIM: ", join(' ', @ts), " > ", join(' ', map { "$_='$attr->{$_}'" } keys %$attr), "\n" if defined $ENV{debug};
+      }
+      $$tt .= "\n\n" if $name eq 'br' and not $trim;
+    }
+    sub text_h {
+      my ($t) = @_;
+      my $ctx = $ts[-1];
+      print STDERR "$trim TEXT[$ctx]: $t\n" if defined $ENV{debug};
+      return if $trim;
+      $t =~ s/^\s+//;
+      $t =~ s/\s+$//;
+      $$tt .= $t;
+      $$tt .= "\n\n" if $ctx =~ /^(div|p)$/;
+    }
+    sub end_h {
+      my $t = shift @ts;
+      if ($t eq '_trim_') {  # Stop trimming, we unrolled up to the corresponding closing tag
+        $trim-- if $trim > 0;
+        shift @ts;
+      }
+    }
+    $p->parse($html);
+    $p->eof;
   } else {
     # Strip oldschool signature (after the '--' line)
     $text =~ s/\n--\s*\n.*/\n/s;
